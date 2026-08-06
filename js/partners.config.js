490fc8d407d2c2a96393a95dc1f9ba39c66f5225
Alexandre Coutinho <alexandre.coutinho81@hotmail.com>
2026-08-06 07:21:16 -0300

    altera banner parceiro

--- js/partners.config.js
+++ js/partners.config.js
@@ -1,8 +1,8 @@
 window.partners = [
   {
     name: 'Corretou',
-    image: 'img/parceiros/corretou.webp',
-    url: 'https://corretou.com.br/?utm_source=comparafinanciamento&utm_medium=banner&utm_campaign=parceria_corretou',
+    image: 'img/parceiros/banner-home-web.webp',
+    url: 'https://consorcio.cotacao.app/?id_site=seguroou&id_parceiro=4348',
     alt: 'Corretou — parceiro do Compare Financiamento',
     active: true,
     order: 1,
